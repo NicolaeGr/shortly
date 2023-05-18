153aaff016914db36115f1448052e9c1215bc85e
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2023-05-18 14:39:00 +0300

    implemented banner section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,5 @@
 <HeroSection />
 
 <ShortenLinksSection />
+
+<BannerSection />
---
+++ src/routes/components/BannerSection.svelte
@@ -0,0 +1,38 @@
+<section>
+  <div class="container">
+    <h2>Boost your links today</h2>
+    <button>Get Started</button>
+  </div>
+</section>
+
+<style>
+  section {
+    position: relative;
+    background-color: var(--color-primary-dark-violet);
+    background-image: url('images/bg-boost-desktop.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .container {
+    flex-direction: column;
+    padding: 120px 0;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+    color: white;
+  }
+
+  button {
+    color: white;
+    font-size: 1rem;
+    font-weight: 700;
+
+    padding: 16px 32px;
+
+    border: none;
+    border-radius: 32px;
+    background-color: var(--color-primary-cyen);
+  }
+</style>
