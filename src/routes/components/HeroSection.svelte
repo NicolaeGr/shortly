4fdf769836c282c95dbad316198cd0d1a7d8fe56
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2023-05-18 14:37:25 +0300

    Implemented desktop version of the hero section

---
+++ src/routes/components/HeroSection.svelte
@@ -0,0 +1,57 @@
+<section>
+  <div class="container">
+    <div class="content">
+      <h1>More then just shorter links</h1>
+      <p>
+        Build your brand's recognition and get detailed insights on how your links are performing
+      </p>
+      <button>Get Started</button>
+    </div>
+    <div class="image_wrapper"><img src="images/illustration-working.svg" alt="" /></div>
+  </div>
+</section>
+
+<style>
+  section {
+    margin: 32px 0px 64px;
+  }
+
+  .container > * {
+    flex: 1 0 55%;
+  }
+
+  .content {
+    flex-direction: column;
+    align-items: start;
+    gap: 24px;
+  }
+
+  h1 {
+    color: var(--color-neutral-very-dark-violet);
+    font-size: 5rem;
+    margin: 0px;
+    line-height: 5rem;
+  }
+
+  p {
+    color: var(--color-neutral-grayish-violet);
+    font-size: 1.5rem;
+    margin: 0px;
+  }
+
+  button {
+    color: white;
+    font-size: 1rem;
+    font-weight: 700;
+
+    padding: 16px 32px;
+
+    border: none;
+    border-radius: 32px;
+    background-color: var(--color-primary-cyen);
+  }
+
+  .image_wrapper {
+    padding-left: 120px;
+  }
+</style>
